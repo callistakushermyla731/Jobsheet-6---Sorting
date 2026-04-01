c87c00658ec11b992bbdc42ecaeae1f2ad21d344
Callista Dinar Kushermyla <callistakushermyla731@gmail.com>
2026-04-01 09:52:09 +0700

    Praktikum 1 - Insertion Sort

--- Praktikum05/SortingMain04.java
+++ Praktikum05/SortingMain04.java
@@ -19,5 +19,14 @@ public class SortingMain04 {
         dataurut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
+
+        int c[] = {40, 10, 4, 9, 3};
+        Sorting04 dataurut3 = new Sorting04(c, c.length);
+
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
     }
 }
